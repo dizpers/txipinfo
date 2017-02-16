23fe2b7d7878d877e379cec2580aae1d9af759ac
dizpers <dizpers@gmail.com>
2017-02-16 18:22:25 +0700

    Perform DNS lookup (for PTR record) & whois lookup

--- txipinfo/server.py
+++ txipinfo/server.py
@@ -1,13 +1,19 @@
 from datetime import datetime
+
 import ipaddress
 
 from klein import Klein
 
-from twisted.internet.defer import succeed, Deferred
-from twisted.web.template import XMLFile, Element, renderer
+from twisted.internet import utils
+from twisted.internet.defer import inlineCallbacks, returnValue
+
+from twisted.names import client
+
 from twisted.python.filepath import FilePath
 
-DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S +0000'
+from twisted.web.template import XMLFile, Element, renderer
+
+DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%S+0000'
 
 app = Klein()
 
@@ -45,18 +51,25 @@ class HistoryElement(Element):
             yield tag.clone().fillSlots(
                 ip=record[0],
                 ptr=record[1],
+                # please, feel free to use the following for pretty printing of whois data
+                # whois=[tags.pre(part) for part in record[2].split('\n')],
                 whois=record[2],
                 datetime=record[3]
             )
 
 
+@inlineCallbacks
 def get_result_for(ip):
-    d = Deferred()
-    ptr = ''
-    whois = ''
+    reversed_name = '.'.join(reversed(ip.split('.'))) + '.in-addr.arpa'
+    # you can simulate latency with
+    # answers, _, _ = yield task.deferLater(reactor, 10.0, client.lookupPointer, reversed_name)
+    # TODO: handle errors
+    answers, _, _ = yield client.lookupPointer(reversed_name)
+    ptr = str(answers[0].payload) if answers else ''
+    # TODO: handle errors (no such command? we're on windows? anything else?)
+    whois = yield utils.getProcessOutput('whois', (ip,))
     datetime_str = datetime.utcnow().strftime(DATETIME_FORMAT)
-    d.callback((ip, ptr, whois, datetime_str))
-    return d
+    returnValue((ip, ptr, whois, datetime_str))
 
 
 @app.route('/')
@@ -65,8 +78,7 @@ def index(request):
     if request.method == 'POST':
         try:
             ip = request.args.get('ip')[0]
-            ip = unicode(ip)
-            if not isinstance(ipaddress.ip_address(ip), ipaddress.IPv4Address):
+            if not isinstance(ipaddress.ip_address(unicode(ip)), ipaddress.IPv4Address):
                 raise ValueError
         except TypeError:
             message = 'No IP provided'
